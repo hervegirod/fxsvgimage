2741e0eb0611666913d9a07ba5ce3397cfaae63e
Herve <herve.girod@dassault-aviation.com>
2021-11-06 00:09:17 +0100

    - Support stroke-dasharray and stroke-dashoffset  - Support the style node  - Avoid throwing an exception if the svg document contains a DTD with reference to an external http source, on a platform which has no internet access  - Support the filter element

---
+++ test/org/girod/javafx/svgimage/xml/SVGStyleBuilderTest.java
@@ -0,0 +1,78 @@
+/*
+Copyright (c) 2021, Hervé Girod
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Alternatively if you have any questions about this project, you can visit
+the project website at the project page on https://github.com/hervegirod/fxsvgimage
+ */
+package org.girod.javafx.svgimage.xml;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @since 0.4
+ */
+public class SVGStyleBuilderTest {
+   
+   public SVGStyleBuilderTest() {
+   }
+   
+   @BeforeClass
+   public static void setUpClass() {
+   }
+   
+   @AfterClass
+   public static void tearDownClass() {
+   }
+   
+   @Before
+   public void setUp() {
+   }
+   
+   @After
+   public void tearDown() {
+   }
+
+   /**
+    * Test of parseStyle method, of class SVGStyleBuilder.
+    */
+   @Test
+   public void testParseStyle() {
+      System.out.println("SVGStyleBuilderTest : testParseStyle");
+      String content = ".st0{fill:#DEDEDE;} .st1{fill:#3E295E;} .st2{fill:#FFFFFF;} .st3{fill:#429946;} .st4{fill:#F5EB30;}";
+      Styles styles = SVGStyleBuilder.parseStyle(content, null);
+      assertNotNull("Styles must not be null", styles);
+   }
+   
+}
