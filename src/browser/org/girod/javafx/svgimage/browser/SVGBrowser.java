c79c4fd8b26d0a469c7787231398a9f136e044e5
Herve <herve.girod@dassault-aviation.com>
2021-11-13 12:57:34 +0100

    handle jpeg images correctly in the browser

--- src/browser/org/girod/javafx/svgimage/browser/SVGBrowser.java
+++ src/browser/org/girod/javafx/svgimage/browser/SVGBrowser.java
@@ -32,6 +32,15 @@ the project website at the project page on https://github.com/hervegirod/fxsvgim
  */
 package org.girod.javafx.svgimage.browser;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferInt;
+import java.awt.image.DirectColorModel;
+import java.awt.image.PixelGrabber;
+import java.awt.image.Raster;
+import java.awt.image.RenderedImage;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
@@ -58,6 +67,7 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
@@ -191,12 +201,32 @@ public class SVGBrowser extends Application {
             file = new File(file.getParentFile(), name);
          }
       }
+      if (ext == null) {
+         ext = "png";
+      } else if (ext.equals("jpeg")) {
+         ext = "jpg";
+      }
 
       SnapshotParameters params = new SnapshotParameters();
+      params.setFill(Color.WHITE);
       WritableImage image = svgImage.snapshot(params, null);
       try {
-         ImageIO.write(SwingFXUtils.fromFXImage(image, null), ext, file);
-      } catch (IOException ex) {
+         RenderedImage awtImg = SwingFXUtils.fromFXImage(image, null);
+         if (ext.equals("jpg")) {
+            // see https://stackoverflow.com/questions/4386446/issue-using-imageio-write-jpg-file-pink-background
+            PixelGrabber pg = new PixelGrabber((java.awt.Image) awtImg, 0, 0, -1, -1, true);
+            pg.grabPixels();
+            int width = pg.getWidth(), height = pg.getHeight();
+
+            int[] RGB_MASKS = { 0xFF0000, 0xFF00, 0xFF };
+            ColorModel RGB_OPAQUE = new DirectColorModel(32, RGB_MASKS[0], RGB_MASKS[1], RGB_MASKS[2]);
+            DataBuffer buffer = new DataBufferInt((int[]) pg.getPixels(), pg.getWidth() * pg.getHeight());
+            WritableRaster raster = Raster.createPackedRaster(buffer, width, height, width, RGB_MASKS, null);
+            awtImg = new BufferedImage(RGB_OPAQUE, raster, false, null);
+         }
+         ImageIO.write(awtImg, ext, file);
+      } catch (IOException | InterruptedException ex) {
+         System.err.println(ex.getMessage());
       }
    }
 
